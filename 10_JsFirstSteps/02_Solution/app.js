03dfc800337747bfbc6d64e03b6e51f1da0bd4d8
RaviHamidov <ravihamidov42@gmail.com>
2022-03-18 19:29:26 +0400

    10_JsFirstSteps > 02_Solutions Completed

---
+++ 10_JsFirstSteps/02_Solution/app.js
@@ -0,0 +1,5 @@
+// Assign a variable named x. Any type of information can be assigned to this variable. All you need to do is find the type of value entered and type a message on the appropriate screen. (Example: The value you enter is a number).
+
+let x = true;
+
+console.log("The value you enter is a " + typeof x);
